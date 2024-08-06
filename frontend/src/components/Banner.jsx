c13f2b4c0f177b0aec9db84e38622a194d023eef
navin-26 <05navin2000@gmail.com>
2024-08-06 19:22:02 +0530

    first-update

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -3,16 +3,26 @@ import banImage from '../assets/ban.jpg';
 
 const Banner = () => {
   return (
-    <section className="relative flex flex-row justify-evenly pl-10 pt-[180px] h-[400px] z-10">
-    
+    <section className="relative flex flex-row justify-evenly pl-10 pt-[180px] h-auto z-10">
       <div className="relative container w-3/4 text-center">
         <h1 className="text-[45px] font-bold mb-4 text-black">Welcome to Skiller E-Learning Platform</h1>
         <p className="text-[25px] text-black font-semibold">Your gateway to a world of knowledge</p>
-
-
+        
+        <div className="overflow-hidden whitespace-nowrap mt-10 text-gray-700 uppercase font-bold">
+          <div className="inline-block animate-marquee">
+            <span className="mx-4">IT & Software</span>
+            <span className="mx-4">Business</span>
+            <span className="mx-4">Office Productivity</span>
+            <span className="mx-4">Personal Development</span>
+            <span className="mx-4">Design</span>
+            <span className="mx-4">Health & Fitness</span>
+            <span className="mx-4">Music</span>
+            <span className="mx-4">Teaching & Academics</span>
+          </div>
+        </div>
       </div>
-      <div className='w-1/3   bg-contain h-28'>
-        <img src={banImage} alt="img" className='border  rounded-l-full' />
+      <div className='w-1/3 bg-contain h-28'>
+        <img src={banImage} alt="img" className='border rounded-l-full' />
       </div>
     </section>
   );
